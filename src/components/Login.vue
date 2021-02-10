093a9a065ca0c5b233215b0cb3eb5a13ced4ef53
Phairat Lin <yangsheng104@gmail.com>
2021-02-10 21:29:15 +0700

    Still working things out

---
+++ src/components/Login.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="info">
+    <p>Click to complete a todo</p>
+    <p>Double-click to edit a todo</p>
+    <p>Created by Phairat Lin!</p>
+    <p>Rapid Front End Project</p>
+  </footer>
+</template>
